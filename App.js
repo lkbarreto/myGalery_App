d7e65db8f7f48fd1e15d63d2618e4360c9d3bacb
lkbarreto <lkbarreto@uninorte.edu.co>
2020-06-25 21:38:19 -0500

    new private view added

--- App.js
+++ App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Card, CardItem, Text, Body } from 'native-base';
-export default class CardHeaderFooterExample extends Component {
+import { Text, View, StyleSheet, Image, Button, Alert, ImageBackground } from 'react-native';
+import AppLayout from './src/appLayout'
+import Login from './src/public/login'
+class MyGallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: "lkbarreto",
+      Pass: "",
+      logged: false
+    }
+  }
+
+  isLogged=() =>{
+    this.setState({
+      logged: true
+    })
+  }
+
   render() {
     return (
-      <Container>
-        <Header />
-        <Content>
-          <Card>
-            <CardItem header>
-              <Text>NativeBase</Text>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Text>
-                  //Your text here
-                </Text>
-              </Body>
-            </CardItem>
-            <CardItem footer>
-              <Text>GeekyAnts</Text>
-            </CardItem>
-         </Card>
-        </Content>
-      </Container>
-    );
+      <View>
+        <Login />
+      </View>
+    )
   }
 }
+
+export default MyGallery 
